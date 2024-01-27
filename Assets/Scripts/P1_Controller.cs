107dcf590e33e0c1cd306f17746039e826c79b2a
2301322 <b.lau@digipen.edu>
2024-01-28 01:13:37 +0800

    Player: Gravity scale added & Hardset z-positions (no more knockback)

--- Assets/Scripts/P1_Controller.cs
+++ Assets/Scripts/P1_Controller.cs
@@ -4,62 +4,81 @@ using UnityEngine;
 
 public class Player1Controller : MonoBehaviour
 {
-    //bools
-    public bool isRunning = false;
-    public bool isPaused = false;
-    public bool isAlive = true;
+	//bools
+	public bool isRunning = false;
+	public bool isPaused = false;
+	public bool isAlive = true;
 
-    //run speed/time
-    public float RunSpeed;
-    //public float RunTime;
-    //velocities
-    public float HorizontalSpeed;
-    //public float VerticalSpeed;
-    float horizontalInput;
-    float verticalInput;
-    public Rigidbody rb;
+	//run speed/time
+	public float RunSpeed;
+	//public float RunTime;
+	//velocities
+	public float HorizontalSpeed;
+	//public float VerticalSpeed;
+	float horizontalInput;
+	float verticalInput;
 
-    [SerializeField] private float Jumpforce = 350;
-    [SerializeField] private LayerMask GroundMask;
+	float gravityScale = 5f; // Change this value to accelerate/decelerate jump & fall speed
+	public static float globalGravity = -9.81f;
+
+	public Rigidbody rb;
+
+	[SerializeField] private float Jumpforce = 350;
+	[SerializeField] private LayerMask GroundMask;
 
 
-    private void Awake()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
+	private void Awake()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
 
-    private void FixedUpdate()
-    {
-        if (isAlive)
-        {
+	private void FixedUpdate()
+	{
+		if (isAlive)
+		{
 /*            Vector3 forwardMovement = transform.forward * RunSpeed * Time.fixedDeltaTime;*/
-            Vector3 horizontalMovement = transform.right * horizontalInput * HorizontalSpeed * Time.fixedDeltaTime;
-            rb.MovePosition(rb.position + horizontalMovement);
-        }
-    }
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+			Vector3 horizontalMovement = transform.right * horizontalInput * HorizontalSpeed * Time.fixedDeltaTime;
 
-    // Update is called once per frame
-    void Update()
-    {
-        horizontalInput = Input.GetAxis("Horizontal");
+			rb.MovePosition(rb.position + horizontalMovement);
 
-        float playerHeight = GetComponent<Collider>().bounds.size.y;
-        bool IsGrounded = Physics.Raycast(transform.position, Vector3.down, (playerHeight / 2) + 0.1f, GroundMask);
+			// Hardset z-position of player to 8
+			rb.transform.position = SetZ(rb.transform.position, 8);
 
-        if (Input.GetKeyDown(KeyCode.W) && isAlive && IsGrounded == true)
-        {
-            Jump();
-        }
-    }
+			// Implementation of gravity with scale
+			Vector3 gravity = globalGravity * gravityScale * Vector3.up;
+			rb.AddForce(gravity, ForceMode.Acceleration);
+		}
+	}
+	
+	// Start is called before the first frame update
+	void Start()
+	{
+		
+	}
 
-    public void Jump()
-    {
-        rb.AddForce(Vector3.up * Jumpforce);
-    }
+	// Update is called once per frame
+	void Update()
+	{
+		horizontalInput = Input.GetAxis("Horizontal");
+
+		float playerHeight = GetComponent<Collider>().bounds.size.y;
+		bool IsGrounded = Physics.Raycast(transform.position, Vector3.down, (playerHeight / 2) + 0.1f, GroundMask);
+
+		if (Input.GetKeyDown(KeyCode.W) && isAlive && IsGrounded == true)
+		{
+			Jump();
+		}
+	}
+
+	public void Jump()
+	{
+		rb.AddForce(Vector3.up * Jumpforce);
+	}
+
+	// Return
+	private Vector3 SetZ(Vector3 vector, float z)
+	{
+		vector.z = z;
+		return vector;
+	}
 }
--- Assets/Scripts/P2_Controller.cs
+++ Assets/Scripts/P2_Controller.cs
@@ -4,62 +4,81 @@ using UnityEngine;
 
 public class Player2Controller : MonoBehaviour
 {
-    //bools
-    public bool isRunning = false;
-    public bool isPaused = false;
-    public bool isAlive = true;
+	//bools
+	public bool isRunning = false;
+	public bool isPaused = false;
+	public bool isAlive = true;
 
-    //run speed/time
-    public float RunSpeed;
-    //public float RunTime;
-    //velocities
-    public float HorizontalSpeed;
-    //public float VerticalSpeed;
-    float horizontalInput;
-    float verticalInput;
-    public Rigidbody rb;
+	//run speed/time
+	public float RunSpeed;
+	//public float RunTime;
+	//velocities
+	public float HorizontalSpeed;
+	//public float VerticalSpeed;
+	float horizontalInput;
+	float verticalInput;
 
-    [SerializeField] private float Jumpforce = 350;
-    [SerializeField] private LayerMask GroundMask;
+	float gravityScale = 5f; // Change this value to accelerate/decelerate jump & fall speed
+	public static float globalGravity = -9.81f;
+
+	public Rigidbody rb;
+
+	[SerializeField] private float Jumpforce = 350;
+	[SerializeField] private LayerMask GroundMask;
 
 
-    private void Awake()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
+	private void Awake()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
 
-    private void FixedUpdate()
-    {
-        if (isAlive)
-        {
-            /*            Vector3 forwardMovement = transform.forward * RunSpeed * Time.fixedDeltaTime;*/
-            Vector3 horizontalMovement = transform.right * horizontalInput * HorizontalSpeed * Time.fixedDeltaTime;
-            rb.MovePosition(rb.position + horizontalMovement);
-        }
-    }
+	private void FixedUpdate()
+	{
+		if (isAlive)
+		{
+			/*            Vector3 forwardMovement = transform.forward * RunSpeed * Time.fixedDeltaTime;*/
+			Vector3 horizontalMovement = transform.right * horizontalInput * HorizontalSpeed * Time.fixedDeltaTime;
+			rb.MovePosition(rb.position + horizontalMovement);
 
-    // Start is called before the first frame update
-    void Start()
-    {
+			// Hardset z-position of player to 8
+			rb.transform.position = SetZ(rb.transform.position, 8);
 
-    }
+			// Implementation of gravity with scale
+			Vector3 gravity = globalGravity * gravityScale * Vector3.up;
+			rb.AddForce(gravity, ForceMode.Acceleration);
+		}
 
-    // Update is called once per frame
-    void Update()
-    {
-        horizontalInput = Input.GetAxis("Horizontal");
 
-        float playerHeight = GetComponent<Collider>().bounds.size.y;
-        bool IsGrounded = Physics.Raycast(transform.position, Vector3.down, (playerHeight / 2) + 0.1f, GroundMask);
+	}
 
-        if (Input.GetKeyDown(KeyCode.I) && isAlive && IsGrounded == true)
-        {
-            Jump();
-        }
-    }
+	// Start is called before the first frame update
+	void Start()
+	{
 
-    public void Jump()
-    {
-        rb.AddForce(Vector3.up * Jumpforce);
-    }
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		horizontalInput = Input.GetAxis("Horizontal");
+
+		float playerHeight = GetComponent<Collider>().bounds.size.y;
+		bool IsGrounded = Physics.Raycast(transform.position, Vector3.down, (playerHeight / 2) + 0.1f, GroundMask);
+
+		if (Input.GetKeyDown(KeyCode.I) && isAlive && IsGrounded == true)
+		{
+			Jump();
+		}
+	}
+
+	public void Jump()
+	{
+		rb.AddForce(Vector3.up * Jumpforce);
+	}
+
+	private Vector3 SetZ(Vector3 vector, float z)
+	{
+		vector.z = z;
+		return vector;
+	}
 }
