929d2059c9c9f253c65ee06baff31c7d2beca7c5
ben-lau <b.lau@digipen.edu>
2024-01-27 18:50:17 +0800

    Improved DestroyObject

--- Assets/Scripts/InstantiateObject.cs
+++ Assets/Scripts/InstantiateObject.cs
@@ -5,10 +5,22 @@ using UnityEngine;
 public class InstantiateObject : MonoBehaviour
 {
     public GameObject objectToSpawn;
+    public float timer = 2.0f;
+
 
     // Update is called once per frame
     void Update()
     {
+        float dt = Time.deltaTime;
+        timer -= dt;
+        Debug.Log(timer);
+
+        if (timer <= 0.0f) {
+            Instantiate(objectToSpawn, transform.position, Quaternion.identity);
+            dt = 0.0f;
+            timer = 2.0f;
+        }
+
         if(Input.GetKeyDown(KeyCode.Space)) {
             Instantiate(objectToSpawn, transform.position, Quaternion.identity);
         }
--- Assets/Scripts/MovementObject.cs
+++ Assets/Scripts/MovementObject.cs
@@ -4,24 +4,11 @@ using UnityEngine;
 
 public class MovementObject : MonoBehaviour
 {
-    public bool isAlive = true;
-    //run speed/time
     public float MoveSpeed;
-    //velocities
-    // public float HorizontalSpeed;
-    public Rigidbody rb;
-
-    private void Awake()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
 
     private void FixedUpdate()
     {
-        if (isAlive){
             Vector3 forwardMovement = transform.forward * MoveSpeed * Time.fixedDeltaTime;
-            rb.MovePosition(rb.position - forwardMovement);
-            // Vector3 horizontalMovement = transform.right *  HorizontalSpeed * Time.fixedDeltaTime;
-        }
+            transform.position -= forwardMovement;
     }
 }
