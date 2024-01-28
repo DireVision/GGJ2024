a1e02fe6655d7bcff7b1d08d9a948a95a90776de
kmsSP <zelknight2001@gmail.com>
2024-01-28 17:32:10 +0800

    restart fix

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -20,6 +20,7 @@ public class LevelManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Time.timeScale = 1;
         //loseSoundEffect = gameObject.GetComponentInChildren<AudioSource>();
     }
 
